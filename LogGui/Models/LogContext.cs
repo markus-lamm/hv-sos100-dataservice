b80460a9bc24d622a114ebe095e165eaf94a0af0
FridsBzz <frli0054@student.hv.se>
2024-02-09 10:32:47 +0100

    Fungerande gui för loggning

---
+++ LogGui/Models/LogContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace LogGui.Models
+{
+    public class LogContext : DbContext
+    {
+        public DbSet<Log> Logs { get; set; }
+        public LogContext(DbContextOptions options) : base(options) { }
+    }
+}
