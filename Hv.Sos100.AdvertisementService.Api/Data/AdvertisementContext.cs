a6eed5d8f61afbcb8733005a65b25d7568e448de
PlexiAndersson <williamandersson2000@gmail.com>
2024-03-13 10:43:11 +0100

    initial

---
+++ Hv.Sos100.AdvertisementService.Api/Data/AdvertisementContext.cs
@@ -0,0 +1,13 @@
+﻿using Hv.Sos100.AdvertisementService.Api.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+
+namespace Hv.Sos100.AdvertisementService.Api.Data
+{
+    public class AdvertisementContext : DbContext
+    {
+        public AdvertisementContext(DbContextOptions<AdvertisementContext> options) : base(options) { }
+        public DbSet<Advertisement> Advertisements { get; set; }
+    }
+}
---
+++ Hv.Sos100.AdvertisementService.Api/Models/Advertisement.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Hv.Sos100.AdvertisementService.Api.Models
+{
+    public class Advertisement
+    {
+        [Key] public int AdvertisementID { get; set; }
+        public DateTime TimeStamp { get; set; }
+        public string? Url { get; set; }
+        public string? ImageLink { get; set; }
+        public int? TotalViews { get; set; }
+    }
+}
