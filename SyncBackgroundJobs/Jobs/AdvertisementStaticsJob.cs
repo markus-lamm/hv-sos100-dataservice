ea39cbbc3bfcb6a7d9d0645258face2cc80798c1
PlexiAndersson <williamandersson2000@gmail.com>
2024-03-15 13:33:18 +0100

    Changes to datastructure, GET and POST implemented.

--- SyncBackgroundJobs/Jobs/AdvertisementStaticsJob.cs
+++ SyncBackgroundJobs/Jobs/AdvertisementStaticsJob.cs
@@ -8,64 +8,33 @@ namespace SyncBackgroundJobs.Jobs
     public class AdvertisementStaticsJob : IJob
     {
         private readonly HttpClient _httpClient;
-        private readonly string _baseURL = "https://informatik6.ei.hv.se/statisticapi/";
+        private readonly string _baseURL = "https://informatik6.ei.hv.se/advertisement/api/Ads/getallads";
         public AdvertisementStaticsJob( HttpClient httpClient)
         {
             _httpClient = httpClient;
         }
         public async Task Execute(IJobExecutionContext context)
         {
-            var DemoObj = DemoData();
-
+            
+            List<AdStatistics> adList = new List<AdStatistics>();
             try
             {
                 _httpClient.BaseAddress = new Uri(_baseURL);
 
-                var result = await _httpClient.PostAsJsonAsync("api/AdStatistics", DemoObj);
+                HttpResponseMessage response = await _httpClient.GetAsync("");
 
-                if (!result.IsSuccessStatusCode)
+                if (response.IsSuccessStatusCode)
                 {
-                    var logger = new LogService();
-
-                    var logResult = await logger.CreateApiLog("Advertisement Sync Job", 3, "Anropet lyckades inte");
-
-                    if (!logResult)
-                    {
-                        logger.CreateLocalLog("Advertisement Sync Job", 3, "Anropet lyckades inte");
-
-                    }
-                }
-
+                    string content = await response.Content.ReadAsStringAsync();
+                    adList = JsonSerializer.Deserialize<List<AdStatistics>>(content, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+                }   
             }
             catch (Exception ex)
             {
                 var logger = new LogService();
 
-                var logResult = await logger.CreateApiLog("Advertisement Sync Job", 3, ex.Message);
-
-                if (!logResult)
-                {
-                    logger.CreateLocalLog("Advertisement Sync Job", 3, ex.Message);
-
-                }
+                await logger.CreateLog("Activity SyncBackgroundJobs", LogService.Severity.Error, ex.Message);
             }
-        }
-
-        public AdStatistics DemoData()
-        {
-            Random rand = new Random();
-
-            AdStatistics d = new AdStatistics();
-            d.TimeStamp = DateTime.Now;
-            d.Clicks = rand.Next(1, 100); // Generate a random integer between 1 and 100 for Clicks
-            d.TotalViews = rand.Next(1, 1000); // Generate a random integer between 1 and 1000 for TotalViews
-            d.FemaleViews = rand.Next(1, 500); // Generate a random integer between 1 and 500 for FemaleViews
-            d.MaleViews = rand.Next(1, 500); // Generate a random integer between 1 and 500 for MaleViews
-            d.Age31To50Views = rand.Next(1, 200); // Generate a random integer between 1 and 200 for Age31To50Views
-            d.Age50PlusViews = rand.Next(1, 200); // Generate a random integer between 1 and 200 for Age50PlusViews
-            d.Age16To30Views = rand.Next(1, 100); // Generate a random integer between 1 and 100 for Age16To30Views
-
-            return d;
         }
     }
 }
---
+++ SyncBackgroundJobs/Model/Activity.cs
@@ -0,0 +1,9 @@
+﻿namespace SyncBackgroundJobs.Model
+{
+    public class Activity
+    {
+        public int ActivityID { get; set; }
+        public DateTime TimeStamp { get; set; }
+        public int CategoryID { get; set; }
+    }
+}
