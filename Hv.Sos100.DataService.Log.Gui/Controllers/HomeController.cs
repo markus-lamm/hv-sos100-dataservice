d0228b1a098a94ac4ebfba2000386b3fdd04c3a4
markus-lamm <markus.lamm@outlook.com>
2024-03-16 11:16:10 +0100

    Remove local login/logout functionality, implmented check for userrole

--- Hv.Sos100.DataService.Log.Gui/Controllers/HomeController.cs
+++ Hv.Sos100.DataService.Log.Gui/Controllers/HomeController.cs
@@ -3,58 +3,43 @@ using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 using Hv.Sos100.SingleSignOn;
 using Hv.Sos100.DataService.Log.Gui.Data;
+using Microsoft.VisualStudio.Web.CodeGenerators.Mvc.Templates.BlazorIdentity.Pages.Manage;
 
 namespace Hv.Sos100.DataService.Log.Gui.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
         private readonly ApiService _apiService;
         private readonly AuthenticationService _authenticationService;
 
-        public HomeController(ILogger<HomeController> logger, ApiService apiService, AuthenticationService authenticationService)
+        public HomeController(ApiService apiService, AuthenticationService authenticationService)
         {
-            _logger = logger;
             _apiService = apiService;
             _authenticationService = authenticationService;
         }
 
         public async Task<IActionResult> Index()
         {
-            var existingSession = await _authenticationService.ResumeSession(controllerBase:this, HttpContext);
-            if (existingSession)
-            {
-                _authenticationService.ReadSessionVariables(controller:this, HttpContext);
-            }
-
-            var logs = new List<Api.Models.Log>();
-
             var isAuthenticated = HttpContext.Session.GetString("IsAuthenticated");
-            
-            if (isAuthenticated == "true")
+            if (isAuthenticated == null)
             {
-                logs = await _apiService.GetLogs() ?? new List<Api.Models.Log>();
+                var existingSession = await _authenticationService.ResumeSession(controllerBase:this, HttpContext);
+
+                if (existingSession == false)
+                {
+                    Redirect("https://informatik5.ei.hv.se/eventivo/Home/Login");
+                }
             }
 
+            var userRole = HttpContext.Session.GetString("UserRole");
+            if (userRole != "Admin")
+            {
+                Redirect("https://informatik5.ei.hv.se/eventivo/Home/Login");
+            }
+
+            var logs = await _apiService.GetLogs() ?? new List<Api.Models.Log>();
             logs = logs.OrderByDescending(l => l.TimeStamp).ToList();
-
             return View(logs);
-        }
-
-        public async Task<IActionResult> Login(string email, string password)
-        {
-            var authenticationResult = await _authenticationService.CreateSession(email, password, controllerBase: this, HttpContext);
-            if (authenticationResult)
-            {
-                _authenticationService.ReadSessionVariables(controller:this, HttpContext);
-            }
-            return RedirectToAction("Index");
-        }
-
-        public IActionResult Logout()
-        {
-            _authenticationService.EndSession(controllerBase: this, HttpContext);
-            return RedirectToAction("Index");
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
