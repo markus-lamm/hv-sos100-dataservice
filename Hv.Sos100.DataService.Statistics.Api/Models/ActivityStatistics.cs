e4899f48a4661c41fac27516621b09fffdb9a644
markus-lamm <markus.lamm@outlook.com>
2024-03-18 15:50:18 +0100

    Refactored Statistics jobs, added project references

--- Hv.Sos100.DataService.Statistics.Api/Models/ActivityStatistics.cs
+++ Hv.Sos100.DataService.Statistics.Api/Models/ActivityStatistics.cs
@@ -6,6 +6,7 @@ namespace Hv.Sos100.DataService.Statistics.Api.Models
     {
         [Key] public int ActivityStatisticsID { get; set; }
         public int ActivityID { get; set; }
+        public int? CategoryID { get; set; }
         public DateTime TimeStamp { get; set; }
         public int? TotalSaved { get; set; }
         public int? FemaleSaved {  get; set; }
@@ -14,7 +15,6 @@ namespace Hv.Sos100.DataService.Statistics.Api.Models
         public int? Age16To30Saved {  get; set; }
         public int? Age31To50Saved { get; set; }
         public int? AgeAbove50Saved { get; set; }
-        public string? Category {  get; set; }
     }
 }
           
--- Hv.Sos100.DataService.Statistics.Api/Models/EventStatistics.cs
+++ Hv.Sos100.DataService.Statistics.Api/Models/EventStatistics.cs
@@ -7,6 +7,7 @@ namespace Hv.Sos100.DataService.Statistics.Api.Models
         [Key] public int EventStatisticsID { get; set; }
         public int EventID { get; set; }
         public int? UserID { get; set; }
+        public int? CategoryID { get; set; }
         public DateTime? TimeStamp { get; set; }
         public int? TotalSignups { get; set; }
         public int? FemaleSignups { get; set; }
@@ -15,6 +16,5 @@ namespace Hv.Sos100.DataService.Statistics.Api.Models
         public int? Age16To30Signups { get; set; }
         public int? Age31To50Signups { get; set; }
         public int? AgeAbove50Signups { get; set; }
-        public string? Category { get; set; }
     }
 }
